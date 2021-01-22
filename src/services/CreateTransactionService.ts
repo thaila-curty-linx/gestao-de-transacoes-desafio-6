429b6d3e8ff272ef27b9a66fa3f26ff620e72413
thailajf <thailajf@hotmail.com>
2021-01-22 13:28:28 -0300

    done

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -4,7 +4,6 @@ import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
 import Category from '../models/Category';
 import TransactionsRepository from '../repositories/TransactionsRepository';
-import transactionsRouter from '../routes/transactions.routes';
 
 interface Request {
   title: string;
